9de7a18ca8d91f9867f4dab0089453c818434f25
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-05 09:46:11 -0800

    Add make_txtar to facilitate gpt help

---
+++ make_txtar.sh
@@ -0,0 +1,19 @@
+rm -rf /tmp/foundtruth
+
+rm -f /tmp/foundtruth.tar
+rm -f /tmp/filelist.txt
+
+
+pytest >pytest.log 2>&1
+rg --no-ignore --files . |
+    grep -iE 'test_skeleton.py|src|pytest.log' |
+    grep -viE '__init__.py' |
+    grep -viE '__pycache' |
+    grep -vF .egg |
+    tee /tmp/filelist.txt
+
+tar -cf /tmp/foundtruth.tar -T /tmp/filelist.txt
+mkdir -p /tmp/foundtruth
+tar xf /tmp/foundtruth.tar -C /tmp/foundtruth
+rg --files /tmp/foundtruth
+txtar-c /tmp/foundtruth | pbcopy
