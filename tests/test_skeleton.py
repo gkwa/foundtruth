c3f8898b67771fbd93634b65cf23376be4b93fec
Taylor Monacelli <taylormonacelli@gmail.com>
2023-12-05 08:37:10 -0800

    Initial tests...some failing

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -23,3 +23,40 @@ def test_main(capsys):
     main(["7"])
     captured = capsys.readouterr()
     assert "The 7-th Fibonacci number is 13" in captured.out
+
+
+def test_main_with_invalid_argument(capsys):
+    """Test CLI with an invalid argument"""
+    with pytest.raises(SystemExit):
+        main(["invalid_argument"])
+    captured = capsys.readouterr()
+    assert "error: unrecognized arguments" in captured.err.lower()
+
+
+def test_main_with_verbose_flag(capsys):
+    """Test CLI with verbose flag"""
+    main(["--verbose", "5"])
+    captured = capsys.readouterr()
+    assert "Starting crazy calculations..." in captured.out
+
+
+def test_main_with_missing_argument(capsys):
+    """Test CLI with missing argument"""
+    with pytest.raises(SystemExit):
+        main([])
+    captured = capsys.readouterr()
+    assert "the following arguments are required: n" in captured.err.lower()
+
+
+def test_main_with_zero_argument(capsys):
+    """Test CLI with zero as an argument"""
+    main(["0"])
+    captured = capsys.readouterr()
+    assert "The 0-th Fibonacci number is 0" in captured.out
+
+
+def test_main_with_large_argument(capsys):
+    """Test CLI with a large argument"""
+    main(["10"])
+    captured = capsys.readouterr()
+    assert "The 10-th Fibonacci number is 55" in captured.out
